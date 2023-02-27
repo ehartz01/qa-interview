049d8cc7eb0c3c468b1150a1932446cc3ecefb56
Ethan Hartzell <ethanhartzell@Ethans-MacBook-Air.local>
2023-02-26 20:14:09 -0500

    added testing

--- qa_interview_test.py
+++ qa_interview_test.py
@@ -1,18 +1,34 @@
 from seleniumbase import BaseCase
+import time
 
 class MyTestClass(BaseCase):
-    def test_basics(self):
-        self.open("https://github.com/")
+	def test_basics(self):
+		self.open("https://github.com/")
+		# after landing on the main page, search "react" in the search bar
+		self.type('input[type="text"]', 'react')
+		self.submit('input[type="text"]')
+		time.sleep(2)
+		# click on the filter dropdown to go through different filters and perform some tesing.
+		for i in range(4,9):
+			self.click_nth_visible_element('a[class="menu-item"]', i)
+			time.sleep(1)
+		# # clear the filter and go to facebook/react repo and verify some page elements
+		self.click_nth_visible_element('a[class="menu-item"]', 0)
+		self.click('a[href="/facebook/react"]') #occasionally fails
 
-        # after landing on the main page, search "react" in the search bar
-        
-        # click on the filter dropdown to go through different filters and perform some tesing.
-
-        # clear the filter and go to facebook/react repo and verfy some page elements
-        
-        # navigate through different tabs of the repo (e.g. Issues, Pull requests, etc.)
-
-        # hover over on the main navigation and verfy the dropdown content (e.g. Product, Solutions, etc)
-
-        ## Extra ##
-        ## write any tests you find interesting, don't go too wild :)
+		# navigate through different tabs of the repo (e.g. Issues, Pull requests, etc.)
+		for tabid in ["issues-tab", "pull-requests-tab", "actions-tab", "projects-tab", "wiki-tab", "code-tab"]:
+			self.click(f'a#{tabid}')
+			time.sleep(1)
+		self.click('a#code-tab')
+		# hover over on the main navigation and verify the dropdown content (e.g. Product, Solutions, etc)
+		self.hover('button[class*="HeaderMenu-link"]:contains("Product")')
+		self.assert_element('a:contains("Actions")')
+		self.hover('button[class*="HeaderMenu-link"]:contains("Solutions")')
+		self.assert_element('a:contains("Enterprise")')
+		self.hover('button[class*="HeaderMenu-link"]:contains("Open Source")')
+		self.assert_element('a:contains("Topics")')
+		## Extra ##
+		## write any tests you find interesting, don't go too wild :)
+		self.click('summary[class="btn-primary btn"]')
+		self.click('button:contains("Codespaces")')
